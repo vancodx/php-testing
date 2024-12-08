7cf2d815f913da056e3a6db7bf21f7dd9beeb95f
Ivan Chistyakov <vancodx@gmail.com>
2024-12-08 22:40:20 +0300

    Added ExampleTest.php and phpunit.xml.dist

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use VanCodX\Testing\PHPUnit\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
